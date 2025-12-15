a0b197627c75c0c1ab97ee9c01bf09c26922b03e
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-15 19:08:33 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#
+#% weight=100 color=#008080 icon="\uf043" block="ePaper"
+# 
 # Test function
-#
-#//% blockId= ePaperTest
-#
-#//% block="test $i"
-#//% $i.min=0 $i.max=9
-#
-def test(a: NumberFormat):
+# 
+#% blockId= ePaperTest
+# 
+#% block="test $i"
+#% $i.min=0 $i.max=9
+# 
+def test(a: number):
     return a + 1
--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-// 
+// % weight=100 color=#008080 icon="\uf043" block="ePaper"
+//  
 //  Test function
-// 
-// //% blockId= ePaperTest
-// 
-// //% block="test $i"
-// //% $i.min=0 $i.max=9
-// 
+//  
+// % blockId= ePaperTest
+//  
+// % block="test $i"
+// % $i.min=0 $i.max=9
+//  
 function test(a: number) {
     return a + 1
 }

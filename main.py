0d51635e2c4ab40b8d0f7a557a71aa17a1337578
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-15 18:46:17 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#
+# Test function
+#
+#   //% blockId= ePaperTest
+#
+#    //% block="test $i"
+#   //$ i.min=0 i.max=9
+#
+def test(a: NumberFormat):
+    return a + 1
---
+++ main.ts
@@ -0,0 +1,12 @@
+// 
+//  Test function
+// 
+//    //% blockId= ePaperTest
+// 
+//     //% block="test $i"
+//    //$ i.min=0 i.max=9
+// 
+function test(a: number) {
+    return a + 1
+}
+

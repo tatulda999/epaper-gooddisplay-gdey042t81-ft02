e04e0c03770e608c5feffd8707c1d3cc16ae9439
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-15 19:28:36 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-
+//% weight=100 color=#008080 icon="\uf043" block="ePaper"
 namespace ePaper {
-    pins.digitalWritePin(DigitalPin.P0, 1);
-    pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
-    pins.spiFormat(8, 3);
-    pins.spiFrequency(1000000);
+
+/**
+ * Test function
+*/
+//% blockId= ePaperTest
+//% block="test $i"
+//$ i.min=0 i.max=9
+export function test(i: number): number {
+    return i + 1
 }
+
+}
+
+pins.digitalWritePin(DigitalPin.P0, 1);
+pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
+pins.spiFormat(8, 3);
+pins.spiFrequency(10000000);

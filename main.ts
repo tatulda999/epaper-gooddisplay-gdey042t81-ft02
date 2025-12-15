bab94d6d6aee904f53e6bcaaba1cb6b7e0482b03
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-15 19:25:27 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-pins.digitalWritePin(DigitalPin.P0, 1);
-pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
-pins.spiFormat(8, 3);
-pins.spiFrequency(1000000);
+
+namespace ePaper {
+    pins.digitalWritePin(DigitalPin.P0, 1);
+    pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
+    pins.spiFormat(8, 3);
+    pins.spiFrequency(1000000);
+}
